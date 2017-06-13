706d3b348e769ac324cbeda70523dfe1995506ef
Raghu Ch <raghu5rvm@gmail.com>
2017-06-13 15:41:13 +0530

    Create grade_partition_new.js

--- beta_v_1.0/scripts/grade_partition_new.js
+++ beta_v_1.0/scripts/grade_partition_new.js
@@ -1,9 +1,9 @@
 function grade_partition_new(Data,Positions){
   let Result=[];
-  for(int i=0;i<Positions.length;i++){
+  for(var i=0;i<Positions.length;i++){
     let maxlimit=Positions[i];
     let sum=0;
-    for(int j=0;j<Data.length;j++){
+    for(var j=0;j<Data.length;j++){
       if(Data[j]<maxlimit){
         sum++;
         delete Data[j];
