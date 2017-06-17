c444aa499c0700395356be8c6b0ad2e2673d6611
VinithKrishnan <vinithk@gmail.com>
2017-06-17 16:57:13 +0530

    Update grade_partition_new.js

--- beta_v_1.0/scripts/grade_partition_new.js
+++ beta_v_1.0/scripts/grade_partition_new.js
@@ -1,17 +1,23 @@
 function grade_partition_new(Data,Positions){
   let Result=[];
   let min=-Infinity;
+  Data.sort();
+  let index=0;
   let cumsum=0;
   for(let i=0;i<Positions.length;i++){
     let maxlimit=Positions[i];
     let sum=0;
-    for(let j=0;j<Data.length;j++){
+    for(let j=index;j<Data.length;j++){
       if(Data[j]<maxlimit  && Data[j]>=min){
         sum++;
         cumsum++;
       }
+      else{
+        break;
+      }
     }
         min=Positions[i];
+        index=index+sum;
     //console.log(Data);
     Result.push(sum);
   }
