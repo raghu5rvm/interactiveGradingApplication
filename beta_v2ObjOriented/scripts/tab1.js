57d22b4855d9d59d197a16fbc21d677ff2ace59e
Raghu Ch <raghu5rvm@gmail.com>
2017-06-28 15:02:43 +0530

    Add files via upload

--- beta_v2ObjOriented/scripts/tab1.js
+++ beta_v2ObjOriented/scripts/tab1.js
@@ -1,14 +1,15 @@
 /***********************************************************************
  * 						Tab 1 functions											
  * *********************************************************************/
+document.getElementById("divInput").style.display="block";
+document.getElementById("divStat").style.display="none";
+document.getElementById("divExport").style.display="none";
 function tab1() {
 	console.log("inside tab1 function");
 	document.getElementById("divInput").style.display="block";
 	document.getElementById("divStat").style.display="none";
-	document.getElementById("divExport").style.display="none";
-	
+	document.getElementById("divExport").style.display="none";		
 	obj.setData();
-
 	document.getElementById("tabInput").style.animation="1s fix linear infinite";
 	document.getElementById("tabStat").style.animation="1s flash linear infinite";
 	
